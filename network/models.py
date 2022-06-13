7037dca986ec7ac2834ae075edb531f80a6ad4c3
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-06-13 17:00:31 +0200

    Post model created and added at Admin. Test with 2 posts

--- network/models.py
+++ network/models.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 
 
 class User(AbstractUser):
     pass
 
     def __str__(self):
-        return f"User number '{self.id}' is '{self.username}'. Last connexion: {self.last_login}."
+        return f"USER NUMBER '{self.id}' is '{self.username}'. LAST CONNEXION: {self.last_login}."
+
+class Post(models.Model):
+    userid = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    post = models.TextField(max_length=500)
+    datetime = models.DateTimeField(default=datetime.now)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"POST NUMBER '{self.id}'. BY USER '{self.userid.username}'. CONTENT '{self.post}'. LIKES: '{self.likes}'. PUBLISHED: '{self.datetime}'."

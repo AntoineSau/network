5bd5c2566d9c6450fa0cd99c221ce08208c57823
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-06-14 20:18:25 +0200

    Posting working and updating Post Model

--- network/views.py
+++ network/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from datetime import datetime
 
 from .models import User, Post
 
@@ -10,11 +11,25 @@ from .models import User, Post
 def index(request):
     # Checking if user sent a post by POST method
     if request.method == "POST":
+        # Get what was posted by the user
         posted = request.POST["posted"]
+
+        
+        # Preparing all fields needed to add an entry to Post Model
+        user = request.user
+        post = posted
+        now = datetime.now()
+        likes = 0
+
+        # Saving the new post in Post Model
+        newpost = Post(userid=user, post=post, datetime=now, likes=likes)
+        newpost.save()
+
+
         return render(request, "network/index.html", {
             # Passing all posts from database (with latest first) to display them later
             "posts": Post.objects.all().order_by('-id'),
-            "posted": posted
+            "posted": posted,
         })
 
 

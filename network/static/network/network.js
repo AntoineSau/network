9f2c4034e5cf1af6f8de24819ef8d3a7e7633305
AntoineSau <69819028+AntoineSau@users.noreply.github.com>
2022-06-22 20:25:42 +0200

    Edit post - content modified but need new form

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -4,16 +4,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // Check if user clicked on edit post
     document.querySelectorAll('.buttonedit').forEach(button => {
 
-        // When a button is clicked, switch to that page
+        // When a button is clicked, change data
         button.onclick = function() {
-            // alert(this.dataset.page);
-
+            
             // Trying to get element by ID and change its properties
-            const elem = document.getElementById(this.dataset.page);
-            elem.innerHTML = `<textarea>${this.dataset.page}</textarea> <br> <button class="buttonsave btn btn-primary mb-2">SAVE</button>`;
+            // Retrieving at the same time the id of the post we want to modify
+            const id = document.getElementById(this.dataset.id);
+            const post = document.getElementById(this.dataset.post);
+            // alert(this.dataset.page);
+            console.log(`Click on post ${this.dataset.post}`);
+            // Change current by pre-fileld text area
+            post.innerHTML = `<textarea>${this.dataset.id}</textarea>`;
+            // Modify text on existing save button 
+            id.innerHTML = `<button data-post="{{post.id}}" data-page="{{post.post}}" class="buttonedit btn btn-secondary mb-2">Save changes in post ${this.dataset.post}</button>`
+            
+            // CREATE NEW FORM WITH FETCH ACTION
 
-            // How do I retreive post's id?
-
+            // Stop forms form submitting -> return false
+            
         }
     })
+
+    
+
 });
